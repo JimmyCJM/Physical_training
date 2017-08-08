1d6dc33088e2623fc8c82173b0cb4871f9e1b5c7
JimmyCJM <chijieming@sina.com>
2017-08-08 00:15:16 -0700

    Add files via upload

---
+++ 170808.js
@@ -0,0 +1,16 @@
+//题目：将一个正整数分解质因数。例如：输入90,打印出90=2335。
+
+function factor(number){
+    var result = [];
+    for(let i=2;i<=number;i++){
+        if(number%i === 0)  {
+            number = parseInt(number/i);
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+console.log(factor(77));
+console.log(factor(90));
+console.log(factor(11111));
