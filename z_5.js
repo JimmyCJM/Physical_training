22d567cdeb575757c0f64db1237207b0ec42aae9
acer-pc <chijieming@sina.com>
2017-08-15 17:27:24 +0800

    z_5

--- z_5.js
+++ z_5.js
@@ -1,10 +1,13 @@
 //题目：一个台阶总共有n级，如果一次可以跳1级，也可以跳2级，求总共有多少总跳法。
 function main(n) {
-    let result = 1;
-    for (let i=n;i>1;i--){
-        result *= i;
+    let arr = [0,1];
+    if(n>1){
+        for (let i=2;i<n+2;i++){
+            arr.push(arr[i-2]+arr[i-1]);
+        }
     }
-    return result;
+    return arr[n+1];
 }
+console.log(main(2));
 console.log(main(4));
 console.log(main(8));
