bbf87844df9e9cd6ca7e3447f33bb002882da9a6
acer-pc <chijieming@sina.com>
2017-08-23 14:12:07 +0800

    170823

---
+++ 170823.js
@@ -0,0 +1,17 @@
+/*给定一个数组，该数组中的每个元素要么是个数组，要么是整数。将其变成一个只包含整数的简单数组。
+给定 [1,2,[1,2]]，返回 [1,2,1,2]。
+给定 [1,2,[1,2],[3,4]]，返回 [1,2,1,2,3,4]。
+给定 [4,[3,[2,[1]]]]，返回 [4,3,2,1]。*/
+function main(arr) {
+    arr = arr.map(value =>{
+        if (value.length !== 1){
+            return value.toString();
+        }
+    })
+    arr = arr.toString().split(",");
+    arr = arr.map(value => parseInt(value));
+    return arr;
+}
+console.log(main([1,2,[1,2]]));
+console.log(main([1,2,[1,2],[3,4]]));
+console.log(main([4,[3,[2,[1]]]]));
