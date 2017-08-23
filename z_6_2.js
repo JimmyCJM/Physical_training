727eae43fafa47b892f2a19f36f79d555c6d57b2
acer-pc <chijieming@sina.com>
2017-08-23 20:24:07 +0800

    g

--- z_6_2.js
+++ z_6_2.js
@@ -1,11 +1,16 @@
 //题目：给定一个字符串，字符串是由1-20中的数字组成，但是字符串漏掉了一个数，找到那个数并打印出来。
 function main(str) {
-    if (str.indexOf('12') === str.lastIndexOf("12")) return "12";
+    let arr = [];
+    let result = 0;
+    for (let i=20;i>0;i--) {
+        if (str.includes(i.toString())) str = str.replace(i.toString());
+        else arr.push(i);
+    }
+    if (arr.toString().length === 2) return arr.toString();
     else {
-        for (let i=0;i<21;i++){
-            if (str.includes(i.toString())) str = str.replace(i.toString());
-            else return i;
-        }
+        result = `${arr[0]}${arr[1]}`;
+        if (parseInt(result)>20) return `${arr[1]}${arr[0]}`;
+        else return result;
     }
 }
 let str1 = "19201234567891011121314151618";
